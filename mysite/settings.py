6da350870a5fce2591e0d1de4889f2e681d4693a
doitsutaguchi <doitsutaguchi@gmail.com>
2021-06-20 20:00:23 +0900

    linebot

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!gucyz9x7!aybdyfub97^1w1*)l!woi&jfhruf%&fhbblag98l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,4 +127,8 @@ except ImportError:
 
 if not DEBUG:
     import django_heroku
+    import os
+    CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
+    CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
+
     django_heroku.settings(locals())
